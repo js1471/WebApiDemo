83da0361866b9cfa2dd613eb66b61634d443472a
USER <USER@DESKTOP-7AHK341>
2021-03-25 12:01:52 +1300

    Add project files.

---
+++ Models/TodoContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApiDemo.Models
+{
+    public class TodoContext : DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Todoitem> TodoItems { get; set; }
+    }
+}
